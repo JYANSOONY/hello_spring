58169a546eaed37d8aef7ce6a36d243f81582597
JYANSOONY <tmdgml2224@gmail.com>
2022-10-17 13:02:45 +0900

    test discard

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
-//testtest
 @Controller
 public class HelloController {
     @GetMapping("hello")
